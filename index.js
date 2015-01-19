a02364406b08b0bbfc8f2f7b56af61d7ca801d4d
Celso Fernandes <fernandes@zertico.com>
2015-01-19 17:07:35 -0200

    Add support for .emberscriptrc file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 var Filter = require('broccoli-filter')
 var emberScript = require('ember-script')
 
@@ -17,7 +19,14 @@ EmberScriptFilter.prototype.targetExtension = 'js'
 EmberScriptFilter.prototype.processString = function (string) {
   var emberScriptOptions = { bare: this.bare, js: true, optimise: false }
   try {
-    return emberScript.em2js(string, emberScriptOptions)
+    var json;
+    if (!fs.existsSync('.emberscriptrc')) {
+      return emberScript.em2js(string, emberScriptOptions)
+    }
+
+    json = fs.readFileSync('.emberscriptrc', 'utf8');
+    json = JSON.parse(json);
+    return emberScript.em2js(string, json)
   } catch (err) {
     err.line = err.location && err.location.first_line
     err.column = err.location && err.location.first_column
